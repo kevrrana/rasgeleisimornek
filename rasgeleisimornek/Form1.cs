40a0f07e7c6e697073b1fc7df8431ca1f4e104ba
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-11-20 10:44:46 +0300

    Add files via upload

---
+++ rasgeleisimornek/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rasgeleisimornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            string aranan=txtAra.Text;
+            bool kontrol=false;
+
+            for(int i = 0; i <listBox1.Items.Count;i++)
+            {
+                 if (aranan == listBox1.Items[i].ToString())
+                {
+                   kontrol=true;
+                    break;
+                }
+                if (kontrol == true)
+                {
+                    MessageBox.Show("Öğrenci bulundu.");
+                }
+         
+                else
+                {
+                    MessageBox.Show("Listede böyle bir oğrenci yok.");
+                }
+                
+              
+                
+            }
+        }
+    }
+}
